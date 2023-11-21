af0f3d51adad0a57fc8f39168442e397d44a7e22
MarcelCzarnikuni <czarnikm2@cu.coventry.ac.uk>
2023-11-21 19:32:15 +0000

    voltex added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>I LOVE LOAM</h1>
+      <h1>VOLTEX Website</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          LOAM BUTTON {count}
-        </button>
         <p>
-          Edit <code>WHAT</code> and save to test LOAM
+          placeholder page for VOLTEX
         </p>
       </div>
     </>
